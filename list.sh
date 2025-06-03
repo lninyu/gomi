f42c694177d1035d87a4457c952d308895938a6f
lninyu <59942911+lninyu@users.noreply.github.com>
2025-06-03 15:16:35 +0900

    読むと病気になります(1000000%💀)

--- list.sh
+++ list.sh
@@ -1,4 +1,3 @@
-# wow many pickles
 function list:new() {
     local -n self="${1:?}"
 
@@ -30,76 +29,80 @@ function list:append() {
 }
 
 function list:insert() {
-    local -i pick="${2:?}"
+    local -i index="${2:?}"
 
     case "${3:?}" in
         -r|--raw)
             local -n self="${1:?}"
             local -ai view=(${self[3]})
 
-            if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick <= ${#view[@]})); then
-                view+=([pick]=${#self[@]} ${#}-3 ${view[@]:pick})
+            if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index <= ${#view[@]})); then
+                view+=([index]=${#self[@]} ${#}-3 ${view[@]:index})
                 self+=("${@:4}" [3]="${view[*]}")
             fi ;;
         *)
             local -n self="${1:?}" data="${3:?}"
             local -ai view=(${self[3]})
 
-            if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick <= ${#view[@]})); then
-                view+=([pick]=${#self[@]} ${#data[@]} ${view[@]:pick})
+            if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index <= ${#view[@]})); then
+                view+=([index]=${#self[@]} ${#data[@]} ${view[@]:index})
                 self+=("${data[@]}" [3]="${view[*]}")
             fi ;;
     esac
 }
 
 function list:update() {
-    local -i pick="${2:?}" size
+    local -i index="${2:?}" size
 
     case "${3:?}" in
         -r|--raw)
             local -n self="${1:?}"
             local -a view=(${self[3]})
 
-            if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
-                if ((0 <= (size = ${#} - 3) && size <= view[pick+1])); then
-                    ((self[2] += view[pick+1] - size))
-                    view[pick+1]=${size}
-                    self+=([view[pick] ]="${4}" "${@:5}" [3]="${view[*]}")
+            if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index < ${#view[@]})); then
+                if ((0 <= (size = ${#} - 3) && size <= view[index+1])); then
+                    ((self[2] += view[index+1] - size))
+                    view[index+1]=${size}
+                    self+=([view[index] ]="${4}" "${@:5}" [3]="${view[*]}")
                 else
-                    ((self[2] += size - view[pick+1]))
-                    view+=([pick]=${#self[@]} ${size})
+                    ((self[2] += size - view[index+1]))
+                    view+=([index]=${#self[@]} ${size})
                     self+=("${@:4}" [3]="${view[*]}")
+                fi
+
+                if ((self[1] && self[1] <= self[2])); then
+                    list:defrag "${1}"
                 fi
             fi ;;
         *)
             local -n self="${1:?}" data="${3:?}"
-            local -ai view=(${self[3]})
+            local -a view=(${self[3]})
 
-            if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
-                if ((0 <= (size = ${#data[@]}) && size <= view[pick+1])); then
-                    ((self[2] += view[pick+1] - size))
-                    view[pick+1]=${size}
-                    self+=([view[pick] ]="${data[0]}" "${data[@]:1}" [3]="${view[*]}")
+            if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index < ${#view[@]})); then
+                if ((0 <= (size = ${#data[@]}) && size <= view[index+1])); then
+                    ((self[2] += view[index+1] - size))
+                    view[index+1]=${size}
+                    self+=([view[index] ]="${data[0]}" "${data[@]:1}" [3]="${view[*]}")
                 else
-                    ((self[2] += size - view[pick+1]))
-                    view+=([pick]=${#self[@]} ${size})
+                    ((self[2] += size - view[index+1]))
+                    view+=([index]=${#self[@]} ${size})
                     self+=("${data[@]}" [3]="${view[*]}")
+                fi
+
+                if ((self[1] && self[1] <= self[2])); then
+                    list:defrag "${1}"
                 fi
             fi ;;
     esac
-
-    if ((self[1] && self[1] <= self[2])); then
-        list:defrag "${1}"
-    fi
 }
 
 function list:remove() {
     local -n self="${1:?}"
-    local -i pick="${2:?}"
+    local -i index="${2:?}"
     local -a view=(${self[3]})
 
-    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
-        ((self[2] += view[pick+1], view[pick+1] = 0))
+    if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index < ${#view[@]})); then
+        ((self[2] += view[index+1], view[index+1] = 0))
         self[3]="${view[*]}"
 
         if ((self[1] && self[1] <= self[2])); then
@@ -110,12 +113,13 @@ function list:remove() {
 
 function list:delete() {
     local -n self="${1:?}"
-    local -i pick="${2:?}"
+    local -i index="${2:?}"
     local -a view=(${self[3]})
 
-    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
-        ((self[2] += view[pick+1]))
-        view+=([pick]="" "")
+    if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index < ${#view[@]})); then
+        ((self[2] += view[index+1]))
+        view+=([index]="" "")
+        view=(${view[*]})
         self[3]="${view[*]}"
 
         if ((self[1] && self[1] <= self[2])); then
@@ -130,24 +134,25 @@ function list:set() {
 
 function list:get() {
     local -n self="${1:?}" data="${3:?}"
-    local -i pick="${2:?}"
+    local -i index="${2:?}"
     local -a view=(${self[3]})
 
-    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
-        data=("${self[@]:view[pick]:view[pick+1]}")
+    if ((self[0] == 1818850164 && 0 <= (index <<= 1) && index < ${#view[@]})); then
+        data=("${self[@]:view[index]:view[index+1]}")
     fi
 }
 
 function list:defrag() {
     local -n self="${1:?}"
-    local -a view=(${self[3]}) temp=()
-    local -i pick size="${#view[@]}"
 
     if ((self[0] == 1818850164)); then
+        local -a view=(${self[3]}) temp=()
+        local -i index size="${#view[@]}"
+
         list_new temp "${self[1]}"
 
-        for ((pick = 0; pick < size; pick += 2)); do
-            temp+=("${self[@]:view[pick]:view[pick+1]}" [3]+="${#temp[@]} ${view[pick+1]} ")
+        for ((index = 0; index < size; index += 2)); do
+            temp+=("${self[@]:view[index]:view[index+1]}" [3]+="${#temp[@]} ${view[index+1]} ")
         done
 
         self=("${temp[@]}")
@@ -156,43 +161,88 @@ function list:defrag() {
 
 function list:squash() {
     local -n self="${1:?}"
-    local -a view=(${self[3]}) temp=()
-    local -i pick size="${#view[@]}"
 
     if ((self[0] == 1818850164)); then
-        for ((pick = 0; pick < size; pick += 2)); do
-            if ((!view[pick+1])); then
-                view+=([pick]="" "")
+        local -a view=(${self[3]})
+        local -i index size="${#view[@]}"
+
+        for ((index = 0; index < size; index += 2)); do
+            if ((!view[index+1])); then
+                view+=([index]="" "")
             fi
         done
 
+        view=(${view[*]})
         self[3]="${view[*]}"
     fi
 }
 
 function list:equals() {
-    local -n self="${1:?}" list="${2:?}"
-    local -a view=(${self[3]}) temp=(${list[3]}) A B
-    local -i pick idx2
+    local -n listA="${1:?}" listB="${2:?}"
+    local -a viewA=(${listA[3]}) viewB=(${listB[3]}) sliceA sliceB
 
-    if ((self[0] == 1818850164 && list[0] == 1818850164 && ${#view[@]} == ${#temp[@]})); then
-        for ((pick = 0; pick < ${#view[@]}; pick += 2)); do
-            if ((view[pick+1] != temp[pick+1])); then
+    if ((listA[0] == 1818850164 && listB[0] == 1818850164 && ${#viewA[@]} == ${#viewB[@]})); then
+        local -i idx1 idx2 va=${#viewA[@]} sa=${#sliceA[@]}
+
+        for ((idx1 = 0; idx1 < va; idx1 += 2)); do
+            if ((viewA[idx1+1] != viewB[idx1+1])); then
                 return 1
             fi
 
-            A=("${self[@]:view[pick]:view[pick+1]}")
-            B=("${list[@]:temp[pick]:temp[pick+1]}")
+            sliceA=("${listA[@]:viewA[idx1]:viewA[idx1+1]}")
+            sliceB=("${listB[@]:viewB[idx1]:viewB[idx1+1]}")
 
-            for ((idx2 = 0; idx2 < ${#A[@]}; ++idx2)); do
-                if [[ "${A[idx2]}" != "${B[idx2]}" ]]; then
+            for ((idx2 = 0; idx2 < sa; ++idx2)); do
+                if [[ "${sliceA[idx2]}" != "${sliceB[idx2]}" ]]; then
                     return 1
                 fi
             done
         done
-
-        return 0
+    else
+        return 1
     fi
+}
 
-    return 1
+function list:forEach() {
+    local -n self="${1:?}"
+    local -r func="${2:?}"
+    local -a view=(${self[3]})
+
+    if ((self[0] == 1818850164)) && declare -F "${func}" &> /dev/null; then
+        for ((index = 0; index < ${#view[@]}; index += 2)); do
+            "${func}" "${self[@]:view[index]:view[index+1]}"
+        done
+    fi
+}
+
+function list:filter() {
+    local -n self="${1:?}" data="${2:?}"
+    local -r func="${3:?}"
+    local -a view=(${self[3]})
+
+    if ((self[0] == 1818850164)) && declare -F "${func}" &> /dev/null; then
+        list:new "${2}"
+
+        for ((index = 0; index < ${#view[@]}; index += 2)); do
+            if "${func}" "${self[@]:view[index]:view[index+1]}"; then
+                data+=("${self[@]:view[index]:view[index+1]}" [3]+="${#data[@]} ${view[index+1]} ")
+            fi
+        done
+    fi
+}
+
+function list:map() {
+    local -n self="${1:?}" data="${2:?}"
+    local -r func="${3:?}"
+    local -a view=(${self[3]}) temp
+
+    if ((self[0] == 1818850164)) && declare -F "${func}" &> /dev/null; then
+        list:new "${2}"
+
+        for ((index = 0; index < ${#view[@]}; index += 2)); do
+            temp=()
+            "${func}" temp "${self[@]:view[index]:view[index+1]}"
+            data+=("${temp[@]}" [3]+="${#data[@]} ${#temp[@]} ")
+        done
+    fi
 }
