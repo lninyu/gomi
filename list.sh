dc48c8ccd6182d0f6923d656327c1b1377afa500
lninyu <59942911+lninyu@users.noreply.github.com>
2025-06-03 13:09:40 +0900

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,263 +1,170 @@
-## data structure
-# list: {
-#     meta: {
-#         type: string,
-#         gcfunc: string,
-#         gcrate: int,
-#         gctick: int,
-#         refs: {
-#             offset: int,
-#             length: int
-#         }[]
-#     },
-#     data: string[]
-# }
-
-declare -r LIST_TYPE="me.lninyu.list"
-
 function list:new() {
-    local -r IFS=$'\x20'
-    local -n list="${1:?}"
-    local -r gcfunc="${2:-"fullgc"}"
-    local -i gcrate="${3:-0}"
+    local -n self="${1:?}"
 
-    if [[ "${list@a}${#list[@]}" == "a0" && "${gcfunc}" =~ ^fullgc$|^defrag$|^squash$ ]]; then
-        list=("${LIST_TYPE}" "${gcfunc}" "$((gcrate < 0 ? 0 : gcrate))" 0)
-        list=("${list[*]}")
-    else
-        return 1
+    if ((!${#self[@]}))
+        then local -i rate="${2//[^0-9]/}"; self=(1818850164 ${rate} 0 "")
+        else return 1
     fi
-}
-
-function list:get() {
-    local -r IFS=$'\x20'
-    local -n result="${1:?}" list="${2:?}"
-    local -a meta=(${list[0]})
-    local -i idx="${3:-0}"
-
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}${result@a}" == "aa" ]] && ((0 <= idx && idx < ${#meta[@]} / 2 - 2)); then
-        result=("${list[@]:meta[idx = idx * 2 + 4]:meta[idx + 1]}")
-    else
-        return 1
-    fi
-}
-
-function list:set() {
-    "${FUNCNAME[0]%:*}:update" "${@}"
 }
 
 function list:append() {
-    local -r IFS=$'\x20'
-    local -i flag=0; [[ "${1}" == "-r" || "${1}" == "--raw" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
+    local -n self="${1:?}"
+    local -a view=(${self[3]})
 
-    if ((flag))
-        then local -a data=("${@:2}")
-        else local -n data="${2:?}"
-    fi
+    case "${2}" in
+        -r|--raw) local -a data=("${@:3}") ;;
+        *) local -n data="${2:?}" ;;
+    esac
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}${data@a}" == "aa" ]]; then
-        meta+=("${#list[@]}" "${#data[@]}")
-        list+=("${data[@]}")
-        list[0]="${meta[*]}"
-    else
-        return 1
+    if ((self[0] == 1818850164)); then
+        view+=("${#self[@]}" "${#data[@]}")
+        self+=("${data[@]}" [3]="${view[*]}")
     fi
 }
 
 function list:insert() {
-    local -r IFS=$'\x20'
-    local -i flag=0; [[ "${1}" == "-r" || "${1}" == "--raw" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
-    local -i idx="${2:-0}"
+    local -n self="${1:?}"
+    local -i pick="${2:?}"
+    local -a view=(${self[3]})
 
-    if ((flag))
-        then local -a data=("${@:3}")
-        else local -n data="${3:?}"
-    fi
+    case "${3}" in
+        -r|--raw) local -a data=("${@:4}") ;;
+        *) local -n data="${3:?}" ;;
+    esac
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}${data@a}" == "aa" ]] && ((0 <= idx && idx < ${#meta[@]} / 2 - 2)); then
-        ((idx = idx * 2 + 4))
-
-        meta+=([idx]="${#list[@]}" "${#data[@]}" "${meta[@]:idx}")
-        list+=("${data[@]}")
-        list[0]="${meta[*]}"
-    else
-        return 1
+    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick <= ${#view[@]})); then
+        view+=([pick]="${#self[@]}" "${#data[@]}" "${view[@]:pick}")
+        self+=("${data[@]}" [3]="${view[*]}")
     fi
 }
 
 function list:update() {
-    local -r IFS=$'\x20'
-    local -i flag=0; [[ "${1}" == "-r" || "${1}" == "--raw" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
-    local -i idx="${2:-0}" size
+    local -n self="${1:?}"
+    local -i pick="${2:?}" size
+    local -a view=(${self[3]})
 
-    if ((flag))
-        then local -a data=("${@:3}")
-        else local -n data="${3:?}"
-    fi
+    case "${3}" in
+        -r|--raw) local -a data=("${@:4}") ;;
+        *) local -n data="${3:?}" ;;
+    esac
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}${data@a}" == "aa" ]] && ((0 <= idx && idx < ${#meta[@]} / 2 - 2)); then
-        ((idx = idx * 2 + 4))
-
-        if ((0 <= ${#data[@]} && ${#data[@]} <= meta[idx + 1])); then
-            meta[idx + 1]="${#data[@]}"
-            list+=([(meta[idx])]="${data[0]}" "${data[@]:1}")
-            list[0]="${meta[*]}"
+    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
+        if ((0 <= (size = ${#data[@]}) && size <= view[pick+1])); then
+            ((self[2] += view[pick+1] - size))
+            view[pick+1]="${size}"
+            self+=([view[pick] ]="${data[0]}" "${data[@]:1}" [3]="${view[*]}")
         else
-            meta[idx + 0]="${#list[@]}"
-            meta[idx + 1]="${#data[@]}"
-            list+=("${data[@]}")
-            list[0]="${meta[*]}"
+            ((self[2] += size - view[pick+1]))
+            view+=([pick]="${#self[@]}" "${size}")
+            self+=("${data[@]}" [3]="${view[*]}")
         fi
-    else
-        return 1
+
+        if ((self[1] && self[1] <= self[2])); then
+            list:defrag "${1}"
+        fi
     fi
 }
 
 function list:remove() {
-    local -r IFS=$'\x20'
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
-    local -i idx="${2:-0}"
+    local -n self="${1:?}"
+    local -i pick="${2:?}"
+    local -a view=(${self[3]})
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]] && ((0 <= idx && idx < ${#meta[@]} / 2 - 2)); then
-        meta[idx * 2 + 5]=0
-        list[0]="${meta[*]}"
-    else
-        return 1
+    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
+        ((self[2] += view[pick+1], view[pick+1] = 0))
+        self[3]="${view[*]}"
+
+        if ((self[1] && self[1] <= self[2])); then
+            list:defrag "${1}"
+        fi
     fi
 }
 
 function list:delete() {
-    local -r IFS=$'\x20'
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
-    local -i idx="${2:-0}"
+    local -n self="${1:?}"
+    local -i pick="${2:?}"
+    local -a view=(${self[3]})
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]] && ((0 <= idx && idx < ${#meta[@]} / 2 - 2)); then
-        meta+=([idx * 2 + 4]="" "")
-        meta=(${meta[*]})
-        list[0]="${meta[*]}"
-    else
-        return 1
+    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
+        ((self[2] += view[pick+1]))
+        view+=([pick]="" "")
+        self[3]="${view[*]}"
+
+        if ((self[1] && self[1] <= self[2])); then
+            list:defrag "${1}"
+        fi
     fi
 }
 
-function list:fullgc() {
-    local -r IFS=$'\x20'
-    local -i flag; [[ "${1}" == "-t" || "${1}" == "--try" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
+function list:set() {
+    list:update "${@}"
+}
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]]; then
-        if ((flag && meta[2] && ++meta[3] < meta[2])); then
-            list[0]="${meta[*]}"
-            return 2
-        fi
+function list:get() {
+    local -n self="${1:?}" data="${3:?}"
+    local -i pick="${2:?}"
+    local -a view=(${self[3]})
 
-        list:squash "${1}"
-        list:defrag "${1}"
-    else
-        return 1
+    if ((self[0] == 1818850164 && 0 <= (pick <<= 1) && pick < ${#view[@]})); then
+        data=("${self[@]:view[pick]:view[pick+1]}")
     fi
 }
 
 function list:defrag() {
-    local -r IFS=$'\x20'
-    local -i flag; [[ "${1}" == "-t" || "${1}" == "--try" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]}) temp=()
-    local -i idx
+    local -n self="${1:?}"
+    local -a view=(${self[3]}) temp=()
+    local -i pick size="${#view[@]}"
 
+    if ((self[0] == 1818850164)); then
+        list_new temp "${self[1]}"
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]]; then
-        if ((flag && meta[2] && ++meta[3] < meta[2])); then
-            list[0]="${meta[*]}"
-            return 2
-        fi
-
-        list:new temp "${meta[1]}" "${meta[2]}"
-
-        for ((idx = 4; idx < ${#meta[@]}; idx+=2)); do
-            meta[idx]="${#temp[@]}"
-            temp+=("${list[@]:meta[idx]:meta[idx + 1]}")
+        for ((pick = 0; pick < size; pick += 2)); do
+            temp+=("${self[@]:view[pick]:view[pick+1]}" [3]+="${#temp[@]} ${view[pick+1]} ")
         done
 
-        list=("${temp[@]}")
-        meta[3]=0
-        list[0]="${meta[*]}"
-    else
-        return 1
+        self=("${temp[@]}")
     fi
 }
 
 function list:squash() {
-    local -r IFS=$'\x20'
-    local -i flag; [[ "${1}" == "-t" || "${1}" == "--try" ]] && { flag=1; shift; }
-    local -n list="${1:?}"
-    local -a meta=(${list[0]})
-    local -i idx
+    local -n self="${1:?}"
+    local -a view=(${self[3]}) temp=()
+    local -i pick size="${#view[@]}"
 
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]]; then
-        if ((flag && meta[2] && ++meta[3] < meta[2])); then
-            list[0]="${meta[*]}"
-            return 2
-        fi
-
-        for ((idx = 4; idx < ${#meta[@]}; idx+=2)); do
-            ((meta[idx + 1])) || meta+=([idx]="" "")
+    if ((self[0] == 1818850164)); then
+        for ((pick = 0; pick < size; pick += 2)); do
+            if ((!view[pick+1])); then
+                view+=([pick]="" "")
+            fi
         done
 
-        meta=(${meta[*]})
-        meta[3]=0
-        list[0]="${meta[*]}"
-    else
-        return 1
-    fi
-}
-
-function list:length() {
-    local -r IFS=$'\x20'
-    local -n result="${1:?}" list="${2:?}"
-    local -a meta=(${list[0]})
-
-    if [[ "${meta[0]}" == "${LIST_TYPE}" && "${list@a}" == "a" ]]; then
-        ((result = ${#meta[@]} / 2 - 2))
-    else
-        return 1
+        self[3]="${view[*]}"
     fi
 }
 
 function list:equals() {
-    local -r IFS=$'\x20'
-    local -n listA="${1:?}" listB="${2:?}"
-    local -a metaA=(${listA[0]}) metaB=(${listB[0]})
-    local -i tmp1 tmp2 size
-    local -a A B
+    local -n self="${1:?}" list="${2:?}"
+    local -a view=(${self[3]}) temp=(${list[3]}) A B
+    local -i pick idx2
 
-    if [[ "${metaA[0]}" != "${LIST_TYPE}" || "${metaB[0]}" != "${LIST_TYPE}" ]]; then
-        return 1
-    fi
+    if ((self[0] == 1818850164 && list[0] == 1818850164 && ${#view[@]} == ${#temp[@]})); then
+        for ((pick = 0; pick < ${#view[@]}; pick += 2)); do
+            if ((view[pick+1] != temp[pick+1])); then
+                return 1
+            fi
 
-    if (((size = ${#metaA[@]}) != ${#metaB[@]})); then
-        return 1
-    fi
+            A=("${self[@]:view[pick]:view[pick+1]}")
+            B=("${list[@]:temp[pick]:temp[pick+1]}")
 
-    for ((tmp1 = 4; tmp1 < size; tmp1 += 2)); do
-        A=("${listA[@]:metaA[tmp1]:metaA[tmp1 + 1]}")
-        B=("${listB[@]:metaB[tmp1]:metaB[tmp1 + 1]}")
-
-        for ((tmp2 = 0; tmp2 < ${#A[@]}; ++tmp2)); do
-            [[ "${A[tmp2]}" != "${B[tmp2]}" ]] && return 1
+            for ((idx2 = 0; idx2 < ${#A[@]}; ++idx2)); do
+                if [[ "${A[idx2]}" != "${B[idx2]}" ]]; then
+                    return 1
+                fi
+            done
         done
-    done
-}
 
-[[ "${BASH_SOURCE[0]}" == "${0}" ]] && function list:test() {
-    :
-} && list:test
+        return 0
+    fi
+
+    return 1
+}
