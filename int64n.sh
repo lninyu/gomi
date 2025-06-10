765d9c7ee2212fdb8006d8d5cfba39b53606210c
lninyu <59942911+lninyu@users.noreply.github.com>
2025-06-11 01:21:07 +0900

    Update int64n.sh
    wipwipiwpiwpiwpiwpiwpipipiwpiwpiwpiwp

--- int64n.sh
+++ int64n.sh
@@ -1,167 +1,198 @@
-declare -ai _MASK_L=(0) _MASK_R=(0); {
-    declare -i i
-    for i in {1..63}; do
-        ((_MASK_L[i] =   (1 <<       i ) - 1 ))
-        ((_MASK_R[i] = ~((1 << (64 - i)) - 1)))
-    done
-    unset i
+function int64n.fill() {
+    local -n array="${1:?}"; array=()
+    local -i count="${2:?}"
 
-    readonly _MASK_L _MASK_R
-}
+    if ((count & __int64n_sign)); then
+        return
+    fi
 
-function int64n:fill() {
-    local -n _0="${1:?}"; _0=()
-    local -i _2=${2//[^0-9]/}
-    local -ai temp=(${3:-0})
-
-    temp+=(${temp[@]} ${temp[@]} ${temp[@]})
-    temp+=(${temp[@]} ${temp[@]} ${temp[@]})
-    temp+=(${temp[@]} ${temp[@]} ${temp[@]}) # 4**3
-
-    until (((_2 -= 64) & 0x8000000000000000)); do
-        _0+=(${temp[@]})
+    until (((count -= 64) & __int64n_sign)); do
+        array+=(${__int64n_fill0[@]})
     done
 
-    _0+=(${temp[@]::64 + _2})
+    array+=(${__int64n_fill0[@]::64 + count})
 }
-function int64n:set() { :;}
-function int64n:get() { :;}
-function int64n:not() {
-    local -n _0="${1:?}" _1="${2:?}"
-    local -i _2=${#_1[@]}
-
-    while ((_2--)); do
-        ((_0[_2] = ~_1[_2]))
-    done
-}
-function int64n:and() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = _1[_3] & _2[_3]))
-    done
-}
-function int64n:or() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = _1[_3] | _2[_3]))
-    done
-}
-function int64n:xor() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = _1[_3] ^ _2[_3]))
-    done
-}
-function int64n:nand() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = ~(_1[_3] & _2[_3])))
-    done
-}
-function int64n:nor() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = ~(_1[_3] | _2[_3])))
-    done
-}
-function int64n:xnor() {
-    local -n _0="${1:?}" _1="${2:?}" _2="${3:?}"
-    local -i _3=$((${#_1[@]} > ${#_2[@]} ? ${#_1[@]} : ${#_2[@]}))
-
-    while ((_3--)); do
-        ((_0[_3] = ~(_1[_3] ^ _2[_3])))
-    done
-}
-function int64n:andAssign() {
-    local -n _0="${1:?}" _1="${2:?}"
-    local -i _2=$((${#_0[@]} > ${#_1[@]} ? ${#_0[@]} : ${#_1[@]}))
-
-    while ((_2--)); do
-        ((_0[_2] &= _1[_2]))
-    done
-}
-function int64n:orAssign() {
-    local -n _0="${1:?}" _1="${2:?}"
-    local -i _2=$((${#_0[@]} > ${#_1[@]} ? ${#_0[@]} : ${#_1[@]}))
-
-    while ((_2--)); do
-        ((_0[_2] |= _1[_2]))
-    done
-}
-function int64n:xorAssign() {
-    local -n _0="${1:?}" _1="${2:?}"
-    local -i _2=$((${#_0[@]} > ${#_1[@]} ? ${#_0[@]} : ${#_1[@]}))
-
-    while ((_2--)); do
-        ((_0[_2] ^= _1[_2]))
-    done
-}
-function int64n:asr() { :;}
-function int64n:asl() { :;}
-function int64n:lsr() {
+function int64n.not() {
     local -n result="${1:?}" array="${2:?}"
-    local -i _shift="${3//[^0-9]/}"
-    local -i shiftW=_shift/64 shiftB=_shift%64
-    local -i i=-1 j=${#array[@]}-shiftW-1
+    local -i size=${#array[@]}
 
-    if ((shiftW)); then
-        result=(${array[@]:shiftW})
-
-        while ((shiftW--)); do
-            result+=(0)
-        done
-    else
-        result=(${array[@]})
-    fi
-
-    if ((shiftB)); then
-        while ((++i < j)); do
-            ((result[i] = result[i] >> shiftB | (result[i+1] & _MASK_L[shiftB]) << (64 - shiftB)))
-        done
-
-        ((result[i] >>= shiftB))
+    while ((size--)); do
+        ((result[size] = ~array[size]))
+    done
+}
+function int64n.and() {
+    if [[ ${1:?} == ${2:?} ]]
+    then int64n._bitwiseAssign "${1}" "${3:?}" "&"
+    else int64n._bitwise "${1}" "${2}" "${3:?}" "&"
     fi
 }
-function int64n:lsl() { :;}
-function int64n:ror() { :;}
-function int64n:rol() { :;}
-function int64n:add() { :;}
-function int64n:sub() { :;}
-function int64n:mul() { :;}
-function int64n:div() { :;}
-function int64n:mod() { :;}
+function int64n.or() {
+    if [[ ${1:?} == ${2:?} ]]
+    then int64n._bitwiseAssign "${1}" "${3:?}" "~"
+    else int64n._bitwise "${1}" "${2}" "${3:?}" "|"
+    fi
+}
+function int64n.xor() {
+    if [[ ${1:?} == ${2:?} ]]
+    then int64n._bitwiseAssign "${1}" "${3:?}" "^"
+    else int64n._bitwise "${1}" "${2}" "${3:?}" "^"
+    fi
+}
+function int64n.nand() {
+    int64n._bitwiseNot "${1:?}" "${2:?}" "${3:?}" "&"
+}
+function int64n.nor() {
+    int64n._bitwiseNot "${1:?}" "${2:?}" "${3:?}" "|"
+}
+function int64n.xnor() {
+    int64n._bitwiseNot "${1:?}" "${2:?}" "${3:?}" "^"
+}
+function int64n.lsr() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}"
+    local -i shift0=shift/64 shift1=shift%64
+}
+function int64n.lsl() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}"
+    local -i shift0=shift/64 shift1=shift%64
+}
+function int64n.asr() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}"
+    local -i shift0=shift/64 shift1=shift%64
+}
+function int64n.asl() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}"
+    local -i shift0=shift/64 shift1=shift%64
+}
+function int64n.ror() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+    local -i shift0=shift/64%size shift1=shift%64
+}
+function int64n.rol() {
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+    local -i shift0=shift/64%size shift1=shift%64
+}
+function int64n.add() { :;}
+function int64n.sub() { :;}
+function int64n.mul() { :;}
+function int64n.div() { :;}
+function int64n.mod() { :;}
+function int64n.ctz() { :;}
+function int64n.clz() { :;}
+function int64n.pop() { :;}
+function int64n.rev() { :;}
+function int64n.set() { :;}
+function int64n.get() { :;}
+function int64n._bitwise() {
+    int64n._internal || return
 
-function array2bits() {
-    local -n array="${1:?}"
-    local -i i=${#array[@]} j
-    local text
+    local -n result="${1:?}" array1="${2:?}" array2="${3:?}"
+    local -i size=$((${#array1[@]} > ${#array2[@]} ? ${#array1[@]} : ${#array2[@]}))
 
-    while ((j = 64, i--)); do
-        while ((j--)); do
-            text+=$((array[i] >> j & 1))
-        done
+    case ${4:?} in
+    "&") while ((size--)); do ((result[size] = array1[size] & array2[size])); done ;;
+    "|") while ((size--)); do ((result[size] = array1[size] | array2[size])); done ;;
+    "^") while ((size--)); do ((result[size] = array1[size] ^ array2[size])); done ;;
+    esac
+}
+function int64n._bitwiseNot() {
+    int64n._internal || return
 
-        text+=$'\n'
+    local -n result="${1:?}" array1="${2:?}" array2="${3:?}"
+    local -i size=$((${#array1[@]} > ${#array2[@]} ? ${#array1[@]} : ${#array2[@]}))
+
+    case ${4:?} in
+    "&") while ((size--)); do ((result[size] = ~(array1[size] & array2[size]))); done ;;
+    "|") while ((size--)); do ((result[size] = ~(array1[size] | array2[size]))); done ;;
+    "^") while ((size--)); do ((result[size] = ~(array1[size] ^ array2[size]))); done ;;
+    esac
+}
+function int64n._bitwiseAssign() {
+    int64n._internal || return
+
+    local -n array1="${1:?}" array2="${2:?}"
+    local -i size=$((${#array1[@]} > ${#array2[@]} ? ${#array1[@]} : ${#array2[@]}))
+
+    case ${4:?} in
+    "&") while ((size--)); do ((array1[size] = array1[size] & array2[size])); done ;;
+    "|") while ((size--)); do ((array1[size] = array1[size] | array2[size])); done ;;
+    "^") while ((size--)); do ((array1[size] = array1[size] ^ array2[size])); done ;;
+    esac
+}
+function int64n._arrayShiftR() {
+    int64n._internal || return
+
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+
+    if ((!shift)); then
+        result=(${array[@]})
+    elif ((0 < shift && shift < size)); then
+        int64n.fill result shift
+        result=(${result[@]::shift} ${array[@]::size - shift})
+    else
+        int64n.fill result size
+    fi
+}
+function int64n._arrayShiftL() {
+    int64n._internal || return
+
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+
+    if ((!shift)); then
+        result=(${array[@]})
+    elif ((0 < shift && shift < size)); then
+        int64n.fill result shift
+        result=(${array[@]:shift} ${result[@]::shift})
+    else
+        int64n.fill result size
+    fi
+}
+function int64n._arrayRotateR() {
+    int64n._internal || return
+
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+
+    if ((shift >= size ? (shift %= size) : (0 < shift))); then
+        result=(${array[@]:size - shift:shift} ${array[@]::size - shift})
+    fi
+}
+function int64n._arrayRotateL() {
+    int64n._internal || return
+
+    local -n result="${1:?}" array="${2:?}"
+    local -i shift="${3:?}" size=${#array[@]}
+
+    if ((shift >= size ? (shift %= size) : (0 < shift))); then
+        result=(${array[@]:shift} ${array[@]::shift})
+    fi
+}
+function int64n._internal() {
+    [[ ${FUNCNAME[2]%.*} == ${__int64n_ns} ]]
+}
+
+function _.int64n() {
+    local -i i=0 fill0=() fill1=() mask=()
+
+    until ((1 << i++ < 0)); do :; done
+    if ((i ^ 64)); then exit 1; fi
+
+    for i in {0..63} ; do
+        ((fill0[i] = 0))
+        ((fill1[i] = ~0))
+        ((mask[i] = i ? (1 << i) - 1 : 0))
     done
 
-    if [[ ${2} ]]; then
-        text="${text//0/${2::1}}"
-        echo -n "${text//1/${2:1:1}}"
-    else
-        echo -n "${text}"
-    fi
-}
-
-declare a=(16 8 4 2 1)
-int64n:lsr a a 65
-array2bits a .+
+    declare -gair __int64n_fill0=("${fill0[@]}")
+    declare -gair __int64n_fill1=("${fill1[@]}")
+    declare -gair __int64n_mask=("${mask[@]}")
+    declare -gir __int64n_sign=0x8000000000000000
+    declare -gr __int64n="${FUNCNAME%.*}"
+} && { _.int64n; unset -f _.int64n;}
