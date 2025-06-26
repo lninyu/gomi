e9bdfb06deb4bddcd460c5390def2cdf9b01a292
lninyu <59942911+lninyu@users.noreply.github.com>
2025-06-27 06:27:22 +0900

    Update double.sh
    fix

--- libs/util/double.sh
+++ libs/util/double.sh
@@ -5,15 +5,15 @@ function double.toRaw() {
 
     printf -v a "%.13a" "${2:?}"; b=${a%p*}
 
-    ((result = ${a::1}1 & 0x8000000000000000 | (${a#*+} + 1023) << 52 | 0x1${b#*.} >> 1))
+    ((result = ${a::1}1 & 0x8000000000000000 | (${a#*+} + 1023) << 52 | 0x1${b#*.} & 0xfffffffffffff))
 }
 
 function double.fromRaw() {
     local -n result="${1:?}"
     local -i a b
 
-    if ((a = ${2:?} << 1 & 0xfffffffffffff, b = (${2} >> 52) - 1023 & 0x7ff, ${2} & 0x8000000000000000))
-        then printf -v result -- "-0x1.%xp+%d" ${a} ${b}
-        else printf -v result "0x1.%xp+%d" ${a} ${b}
+    if ((a = ${2:?} & 0xfffffffffffff, b = ((${2} >> 52) - 1023) & 0x7ff, ${2} & 0x8000000000000000))
+        then printf -v result -- "-0x1.%.13xp+%d" ${a} ${b}
+        else printf -v result "0x1.%.13xp+%d" ${a} ${b}
     fi
 }
